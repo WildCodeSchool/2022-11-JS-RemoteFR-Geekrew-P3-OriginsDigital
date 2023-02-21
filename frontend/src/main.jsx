7a6d94af10ba2efcda14ff29ec4903563264b0a2
fturello <92311869+fturello@users.noreply.github.com>
2023-02-21 17:25:47 +0100

    sass and variables added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "./styles/index.css";
+import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
