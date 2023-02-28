973a94b364f85ba654652442fcc57af5cb6337c7
Julien879465 <beillonnet@laposte.net>
2023-02-28 15:48:19 +0100

    add favorite routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 const videoControllers = require("./controllers/videoControllers");
+const favoriteControllers = require("./controllers/favoriteControllers");
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
@@ -17,4 +18,9 @@ router.put("/video/:id", videoControllers.edit);
 router.post("/video", videoControllers.add);
 router.delete("/video/:id", videoControllers.destroy);
 
+router.get("/favorite", favoriteControllers.browse);
+router.get("/favorite/:id", favoriteControllers.read);
+router.put("/favorite/:id", favoriteControllers.edit);
+router.post("/favorite", favoriteControllers.add);
+router.delete("/favorite/:id", favoriteControllers.destroy);
 module.exports = router;
