a6e8d02c30249511abde26ca107440d80a0b0019
Julien879465 <beillonnet@laposte.net>
2023-02-28 15:15:20 +0100

    add video routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,11 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
+const videoControllers = require("./controllers/videoControllers");
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+router.get("/video", videoControllers.browse);
+router.get("/video/:id", videoControllers.read);
+router.put("/video/:id", videoControllers.edit);
+router.post("/video", videoControllers.add);
+router.delete("/video/:id", videoControllers.destroy);
 
 module.exports = router;
--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import { React } from "react";
+
+import styles from "../styles/Favorites.module.scss";
 
 export default function Favorites() {
-  return <div>Favorites</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>Favorites videos</div>
+    </div>
+  );
 }
