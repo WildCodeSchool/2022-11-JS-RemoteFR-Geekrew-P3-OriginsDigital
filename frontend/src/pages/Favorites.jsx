c88560d0af007d655b54cf63f08453851e1579f7
fturello <92311869+fturello@users.noreply.github.com>
2023-03-02 11:52:44 +0100

    favorite page

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,11 +1,50 @@
-import { React } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { TrashBinOutline } from "react-ionicons";
 
 import styles from "../styles/Favorites.module.scss";
 
 export default function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+  console.log("favorites ==", favorites);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/video`)
+      .then((res) => res.data)
+      .then((data) => {
+        // console.log(data);
+        setFavorites(data);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>Favorites videos</div>
+      <div className={styles.favorites}>
+        {favorites.map((favorite) => (
+          <>
+            <img
+              key={favorite.id}
+              src={favorite.thumbnail}
+              alt={favorite.description}
+            />
+            <div className={styles["title-container"]}>
+              <p>
+                {favorite.title}
+                <div className={styles.trashbin}>
+                  <TrashBinOutline
+                    color="#00000"
+                    // title={}
+                    height="25px"
+                    width="25px"
+                  />
+                </div>
+              </p>
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
