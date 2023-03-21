928406762fed61d43eafabafa8b68203bb30c37f
EmiLef08 <emilef08@gmail.com>
2023-03-21 15:44:22 +0100

    Desk Navbar Created

--- frontend/src/components/DeskNavbar.jsx
+++ frontend/src/components/DeskNavbar.jsx
@@ -1,22 +1,33 @@
 import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.svg";
 
 import styles from "../styles/DeskNavbar.module.scss";
 
 export default function DeskNavbar() {
+  const navigate = useNavigate();
+
+  const handleSignin = () => {
+    navigate("/sign-in");
+  };
+
   return (
     <nav className={styles.contnav}>
       <Link to="/" className={styles.logonav}>
         <img className={styles.logohead} src={logo} alt="logo" />
-        Origins Digital
       </Link>
       <div className={styles.navlink}>
         <ul>
           <li className={styles.link}>
-            <NavLink to="/">Catégorie</NavLink>
+            <NavLink to="/search">Categories</NavLink>
+          </li>
+          <li className={styles.link}>
+            <NavLink to="/help">Questions?</NavLink>
           </li>
         </ul>
+        <button className={styles.btnlog} type="button" onClick={handleSignin}>
+          SIGN IN
+        </button>
       </div>
     </nav>
   );
