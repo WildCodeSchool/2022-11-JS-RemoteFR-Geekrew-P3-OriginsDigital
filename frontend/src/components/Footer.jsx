01e237cb9596b4b5e390610b51c4e4ccff53bc31
EmiLef08 <emilef08@gmail.com>
2023-03-22 09:36:42 +0100

    Footer progress

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,15 +1,33 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import "../styles/Footer.module.scss";
 
 function Footer() {
   return (
     <div className="container">
-      <div className="follow">
-        <p className="follow-title">Follow us</p>
-      </div>
       <div className="origins">
-        <p className="origins-title">Origins Digital ©</p>
+        <p className="origins-title">© Origins Digital</p>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <NavLink to="/legal">Legal Space</NavLink>
+          </li>
+          <li>
+            <a href="https://www.euromediagroup.com/home/">
+              <img
+                src="../assets/EMG_Logo_RGB_White_ROB_Website.svg"
+                alt="EMG Logo"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/company/origins-digital-emg/">
+              <img src="../assets/174857.svg" alt="LinkedIn logo" />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
