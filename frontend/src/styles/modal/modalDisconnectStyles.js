6434e6e47b9e75cfe41fd5f9a327f8614f811ab6
fturello <92311869+fturello@users.noreply.github.com>
2023-03-23 16:59:52 +0100

    responsive + home

--- frontend/src/styles/modal/modalDisconnectStyles.js
+++ frontend/src/styles/modal/modalDisconnectStyles.js
@@ -1,95 +1,109 @@
 const newLocal = `
-            .modal-overlay {
-              position: fixed;
-              top: 0;
-              left: 0;
-              width: 100%;
-              height: 100%;
-              z-index: 250;
-              backdrop-filter: blur(2px);
-              background-color: rgba(0, 0, 0, 0.8);
-            }
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 250;
+  backdrop-filter: blur(2px);
+  background-color: rgba(0, 0, 0, 0.8);
+}
 
-            .modal-wrapper {
-              position: fixed;
-              top: 0;
-              left: 0;
-              z-index: 100;
-              width: 100%;
-              height: 100%;
-              overflow-x: hidden;
-              overflow-y: auto;
-              outline: 0;
-              display: flex;
-              align-items: center;
-            }
+.modal-wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: auto;
+  outline: 0;
+  display: flex;
+  align-items: center;
+}
 
-            .modal {
-              z-index: 100;
-              background: #0a0b0bcc;
-              position: relative;
-              margin: auto;
-              border: 0.15rem solid #fc4f0c;
-              border-radius: 15px;
-              max-width: 500px;
-              width: 80%;
-              padding: 1rem;
-            }
+.modal {
+  z-index: 100;
+  background: #232526;
+  position: relative;
+  margin: auto;
+  border: 0.15rem solid #d16208;
+  border-radius: 15px;
+  max-width: 500px;
+  width: 80%;
+  padding: 1rem;
+}
 
-            .modal-header {
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-            }
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
-            .modal-name {
-              align-content: center;
-              text-align: center;
-              color: #ff7402;
-              font-size: large;
-            }
+.modal-name {
+  align-content: center;
+  text-align: center;
+  color: #d16208;
+  font-size: large;
+  font-weight: bold;
+}
 
-            .modal-close-button {
-              font-size: 1.4rem;
-              font-weight: 700;
-              color: #fc4f0c;
-              cursor: pointer;
-              border: none;
-              background: transparent;
-            }
+.modal-close-button {
+  font-size: 1.4rem;
+  font-weight: 700;
+  color: #d16208;
+  cursor: pointer;
+  border: none;
+  background: transparent;
+}
 
-            .modal-body {
-              margin: 1rem;
-              font-family: "Noto Sans", sans-serif;
-              font-size: 1rem;
-              color: rgb(255, 255, 255);
-              line-height: 1.6rem;
-              letter-spacing: 0.05rem;
-            }
+.modal-body {
+  margin: 1rem;
+  font-family: "Noto Sans", sans-serif;
+  font-size: 1rem;
+  color: rgb(255, 255, 255);
+  line-height: 1.6rem;
+  letter-spacing: 0.05rem;
+}
 
-            .buttons-confirm {
-              display: flex;
-              justify-content: space-around;
-              padding: 0.5rem;
-            }
+.buttons-confirm {
+  display: flex;
+  justify-content: space-around;
+  padding: 0.5rem;
+}
+.buttons-confirm {
+  display: flex;
+  justify-content: space-around;
+  padding: 1rem;
+}
 
-            .yes {
-              color: green;
-              font-size: 1.1rem;
-              background-color: transparent;
-              border: none;
-            }
+.yes,
+.no {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  background: none;
+  border-radius: 0.7rem;
+  border: solid 1px #ff7402;
+  color: #ff7402;
+}
 
-            .no {
-              color: red;
-              font-size: 1.1rem;
-              background-color: transparent;
-              border: none;
-            }
+.yes {
+  color: #fff;
+}
 
-            .yes:hover, .no:hover {
-              cursor: pointer;
-            }
+.no {
+  color: #fff;
+}
+
+.yes:hover,
+.no:hover {
+  cursor: pointer;
+  transform: scale(1.05);
+}
           `;
 
 export default newLocal;
